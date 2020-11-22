015de203f1ba8472d3e27f0f8eb62912b2822bfc
Luvkitri <janjedrzejewski@gmail.com>
2020-11-23 00:24:03 +0100

    New cards enpoints refactor

--- routes/cards.js
+++ routes/cards.js
@@ -6,12 +6,42 @@ const models = require('../models');
 // @route GET /boards/:boardId/lists/:listId/cards
 router.get('/', async (req, res) => {
     try {
+        // check if parms exists
+        if (!req.boardId && !req.listId) {
+            res.status(404).send({ error: "Board id was not provided" });
+        }
+
+        const userId = req.user.id;
+        const boardId = req.boardId;
+        const listId = req.listId;
+
+        const board = await models.Board.findByPk(boardId, {
+            raw: true,
+            include: [{
+                model: models.User,
+                as: 'UsersInBoard',
+                where: {
+                    id: userId
+                }
+            }]
+        });
+
+        // Chcek if board exists
+        if (!board) {
+            res.status(404).send({ error: "Board does not exist" });
+        }
+
+        // Check if user has permission to access this board
+        if (!board['Board.UsersInBoard.UserBoardRelation.read']) {
+            res.status(403).send({ error: "No access to this board" });
+        }
+
         const results = await models.Card.findAll({
             raw: true,
             include: [{
                 model: models.List,
                 where: {
-                    id: req.listId
+                    id: listId
                 }
             }]
         });
@@ -41,6 +71,38 @@ router.get('/', async (req, res) => {
 // @route GET /boards/:boardId/lists/:listId/cards/:cardId
 router.get('/:cardId', async (req, res) => {
     try {
+        const userId = req.user.id;
+        const cardId = req.params.cardId
+
+        const results = await models.Card.findByPk(cardId, {
+            raw: true,
+            include: [{
+                model: models.List,
+                as: 'List',
+                include: [{
+                    model: models.Board,
+                    as: 'Board',
+                    include: [{
+                        model: models.User,
+                        as: 'UsersInBoard',
+                        where: {
+                            id: userId
+                        }
+                    }]
+                }]
+            }]
+        });
+
+        // Chcek if card exists
+        if (!results) {
+            res.status(404).send({ error: "Card does not exist" });
+        }
+
+        // Check if user has permission to access this board
+        if (!results['Board.UsersInBoard.UserBoardRelation.read']) {
+            res.status(403).send({ error: "No access to this board" });
+        }
+
         const results = await models.Card.findByPk(req.params.cardId);
 
         let card = {
@@ -62,11 +124,52 @@ router.get('/:cardId', async (req, res) => {
 // @desc Get card by id
 // @route DELETE /boards/:boardId/lists/:listId/cards/:cardId
 router.delete('/:cardId', async (req, res) => {
-    //TODO: check if card is archived, if not then refuse action
     try {
+        const userId = req.user.id;
+        const cardId = req.params.cardId;
+
+        const results = await models.Card.findByPk(cardId, {
+            raw: true,
+            include: [{
+                model: models.List,
+                as: 'List',
+                include: [{
+                    model: models.Board,
+                    as: 'Board',
+                    include: [{
+                        model: models.User,
+                        as: 'UsersInBoard',
+                        where: {
+                            id: userId
+                        }
+                    }]
+                }]
+            }]
+        });
+
+        // Check if card exists
+        if (!results) {
+            res.status(404).send({ error: "Card does not exist" });
+        }
+
+        // Check if user has permission to access this board
+        if (!results['Board.UsersInBoard.UserBoardRelation.read']) {
+            res.status(403).send({ error: "No access to this board" });
+        }
+
+        // Check if user has rights to edit this board
+        if (!results['UsersInBoard.UserBoardRelation.write']) {
+            res.status(403).send({ error: "User doesn't have rights to edit this board" });
+        }
+
+        // Check if card has been archived befor deleting
+        if (!results.archived) {
+            res.status(405).send({ error: "This card cannot be deleted, it hasn't been archived" });
+        }
+        
         await models.Card.destroy({
             where: {
-                id: req.params.cardId
+                id: cardId
             }
         });
 
@@ -83,10 +186,58 @@ router.delete('/:cardId', async (req, res) => {
 // @route POST /boards/:boardId/lists/:listId/cards/add
 router.post('/add', async (req, res) => {
     try {
+        const userId = req.user.id;
         const cardData = req.body;
+
         // if board_id not specified in request body, but given in url
         if (!cardData.list_id && req.listId) {
             cardData.list_id = req.listId;
+        } else if (!cardData.list_id && !req.listId) {
+            res.status(404).send({ error: "Board id was not provided" });
+        }
+
+        const results = await models.List.findByPk(cardData.list_id, {
+            raw: true,
+            include: [{
+                model: models.Board,
+                as: 'Board',
+                include: [{
+                    model: models.User,
+                    as: 'UsersInBoard',
+                    where: {
+                        id: userId
+                    }
+                }]
+            }]
+        });
+
+        // Chcek if card exists
+        if (!results) {
+            res.status(404).send({ error: "Card does not exist" });
+        }
+
+        // Check if user has permission to access this board
+        if (!results['Board.UsersInBoard.UserBoardRelation.read']) {
+            res.status(403).send({ error: "No access to this board" });
+        }
+
+        // Check if user has rights to edit this board
+        if (!results['UsersInBoard.UserBoardRelation.write']) {
+            res.status(403).send({ error: "User doesn't have rights to edit this board" });
+        }
+
+        const cards = await models.Card.findAll({
+            raw: true,
+            include: [{
+                model: models.List,
+                where: {
+                    id: cardData.list_id
+                }
+            }]
+        });
+
+        if (!cardData.position) {
+            cardData.position = cards.length + 1;
         }
 
         const newCard = models.Card.build(cardData);
@@ -99,49 +250,62 @@ router.post('/add', async (req, res) => {
     }
 });
 
-// @desc Archive card by id
-// @route POST /boards/:boardId/lists/:listId/cards/archive
-router.post('/archive', async (req, res) => {
+// @desc Update card by id
+// @route PUT /cards/:cardId
+router.put('/:cardId', async (req, res) => {
     try {
-        let cardId = req.body.id;
+        const userId = req.user.id;
+        const cardId = req.params.cardId;
 
-        await models.Card.update({
-            archived: true
-        }, {
+        const results = await models.Card.findByPk(cardId, {
+            raw: true,
+            include: [{
+                model: models.List,
+                as: 'List',
+                include: [{
+                    model: models.Board,
+                    as: 'Board',
+                    include: [{
+                        model: models.User,
+                        as: 'UsersInBoard',
+                        where: {
+                            id: userId
+                        }
+                    }]
+                }]
+            }]
+        });
+
+        // Check if card exists
+        if (!results) {
+            res.status(404).send({ error: "Card does not exist" });
+        }
+
+        // Check if user has permission to access this board
+        if (!results['Board.UsersInBoard.UserBoardRelation.read']) {
+            res.status(403).send({ error: "No access to this board" });
+        }
+
+        // Check if user has rights to edit this board
+        if (!results['UsersInBoard.UserBoardRelation.write']) {
+            res.status(403).send({ error: "User doesn't have rights to edit this board" });
+        }
+
+        const updateObject = _.omitBy(req.body, _.isNil);
+
+        await models.List.update(
+            updateObject, {
             where: {
-                id: cardId
+                id: listId
             }
         });
 
-        res.status(200).send({
-            result: true
-        });
+        const updatedList = await models.newList.findByPk(listId, { raw: true });
+        res.status(200).json(updatedList);
     } catch (error) {
-        console.error(error.message);
-        res.status(500).send(error.message);
-    }
-});
-
-// @desc Restore card by id
-// @route POST /boards/:boardId/lists/:listId/cards/restore
-router.post('/restore', async (req, res) => {
-    try {
-        let cardId = req.body.id;
-
-        await models.Card.update({
-            archived: false
-        }, {
-            where: {
-                id: cardId
-            }
+        res.status(500).send({
+            error: error.message
         });
-
-        res.status(200).send({
-            result: true
-        });
-    } catch (error) {
-        console.error(error.message);
-        res.status(500).send(error.message);
     }
 });
 
