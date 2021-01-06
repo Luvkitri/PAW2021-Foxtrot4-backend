f117c4718cd89afa90134b7b97c494eb39b6c2d9
patryk-le <58395545+patryk-le@users.noreply.github.com>
2021-01-06 17:32:06 +0100

    desc of activity

--- routes/activities.js
+++ routes/activities.js
@@ -25,14 +25,45 @@ router.get('/board/:boardId', async (req, res) => {
             raw: true,
         });
         const users = await models.User.findAll();
+        const boards= await models.Board.findAll();
+        const lists= await models.List.findAll();
         let x = []
-        results.forEach((a) => {
+       
+        results.forEach( async (a) => {
             let user = users.find(u => u.id === a.user_id);
+            
             
             let temp = a;
             temp.username = user.first_name + ' ' + user.last_name;
+            if(a.board_id && !a.list_id && !a.card_id && !a.comment_id){
+                let board= await models.Board.findOne({
+                    where:{
+                        id: a.board_id
+                    }
+                })
+                temp.name= board.board_name
+            }else if(a.card_id){
+                let card= await models.Card.findOne({
+                    where:{
+                        id: a.card_id
+                    }
+                })
+                temp.name= card.card_name
+            }else if(a.list_id){
+                let list= await models.List.findOne({
+                    where:{
+                        id: a.list_id
+                    }
+                })
+                temp.name= list.list_name
+            }else{
+                temp.name=""
+            }
+            
             x.push(temp)
+            console.log(x)
         });
+    
         res.status(200).json(x);
     } catch (error) {
         console.error(error.message);
